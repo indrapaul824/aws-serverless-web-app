ed50934ab03c808c11ccdfc00459b77b4d9cc9b1
IndraP24 <indrashis985@gmail.com>
2022-02-17 10:24:19 +0530

    Add the js files for the functionalities

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+  cognito: {
+    userPoolId: "us-east-1_incQb0fZN", // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: "7785ud4r904epeaif3gqtkgbt6", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    region: "us-east-1", // e.g. us-east-2
+  },
+  api: {
+    invokeUrl: "", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+  },
+};
